81576628e33faa95620f983d71a49c276d9c1043
Matt Worrall <matt@lotuselise.org.uk>
2021-10-26 22:03:14 +0100

    Started initial build

---
+++ src/components/FullHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FullHeader.scss';
+
+function FullHeader() {
+  return (
+    <header className="header-full"></header>
+  );
+}
+
+export default FullHeader;
---
+++ src/components/HomeCarousel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomeCarousel.scss';
+
+function HomeCarousel() {
+  return (
+    <div className="home-carousel"></div>
+  );
+}
+
+export default HomeCarousel;
